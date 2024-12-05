8bd1f2b6fbbad86addc1e9e9f61b9cde7be6bc12
REBECA <rebeca.costache@yahoo.com>
2024-12-05 13:13:41 +0200

    changed labelling of data for OneHotEncoder: train_acc=32,4%, test_acc=35,3%

--- ModificariDate.py
+++ ModificariDate.py
@@ -8,7 +8,7 @@ class ModificariDate:
         if not os.path.exists('cat_data_preprocesat.xlsx'):
             self.df = self.__citire_set_de_date__('cat_data.xlsx', sheet_name='Data')
             self.df = self.__codificare__(self.df)
-            self.df = self.__inlocuieste_zero_cu_media__(self.df)
+            self.df = self.__inlocuieste_minus1_cu_media__(self.df)
             self.__scriere_set_de_date__(self.df, 'cat_data_preprocesat.xlsx')
         else:
             self.df = self.__citire_set_de_date__('cat_data_preprocesat.xlsx', sheet_name='Sheet1')
@@ -29,48 +29,48 @@ class ModificariDate:
         return df
     
     def __codificare__(self, df):
-        df['Sexe'] = df['Sexe'].replace('F', 1)
-        df['Sexe'] = df['Sexe'].replace('M', 2)
+        df['Sexe'] = df['Sexe'].replace('F', 0)
+        df['Sexe'] = df['Sexe'].replace('M', 1)
 
-        df['Age'] = df['Age'].replace('Moinsde1', 1)
-        df['Age'] = df['Age'].replace('1a2', 2)
-        df['Age'] = df['Age'].replace('2a10', 3)
-        df['Age'] = df['Age'].replace('Plusde10', 4)
+        df['Age'] = df['Age'].replace('Moinsde1', 0)
+        df['Age'] = df['Age'].replace('1a2', 1)
+        df['Age'] = df['Age'].replace('2a10', 2)
+        df['Age'] = df['Age'].replace('Plusde10', 3)
 
-        df['Race'] = df['Race'].replace('BEN', 1)
-        df['Race'] = df['Race'].replace('SBI', 2)
-        df['Race'] = df['Race'].replace('BRI', 3)
-        df['Race'] = df['Race'].replace('CHA', 4)
-        df['Race'] = df['Race'].replace('EUR', 5)
-        df['Race'] = df['Race'].replace('MCO', 6)
-        df['Race'] = df['Race'].replace('PER', 7)
-        df['Race'] = df['Race'].replace('RAG', 8)
-        df['Race'] = df['Race'].replace('SPH', 9)
-        df['Race'] = df['Race'].replace('ORI', 10)
-        df['Race'] = df['Race'].replace('TUV', 11)
-        df['Race'] = df['Race'].replace('Autre', 12)
-        df['Race'] = df['Race'].replace('NR', 13)
-        df['Race'] = df['Race'].replace('SAV', 14)
+        df['Race'] = df['Race'].replace('BEN', 0)
+        df['Race'] = df['Race'].replace('SBI', 1)
+        df['Race'] = df['Race'].replace('BRI', 2)
+        df['Race'] = df['Race'].replace('CHA', 3)
+        df['Race'] = df['Race'].replace('EUR', 4)
+        df['Race'] = df['Race'].replace('MCO', 5)
+        df['Race'] = df['Race'].replace('PER', 6)
+        df['Race'] = df['Race'].replace('RAG', 7)
+        df['Race'] = df['Race'].replace('SPH', 8)
+        df['Race'] = df['Race'].replace('ORI', 9)
+        df['Race'] = df['Race'].replace('TUV', 10)
+        df['Race'] = df['Race'].replace('Autre', 11)
+        df['Race'] = df['Race'].replace('NR', 12)
+        df['Race'] = df['Race'].replace('SAV', 13)
 
         df['Nombre'] = df['Nombre'].replace('Plusde5', 6)
 
-        df['Logement'] = df['Logement'].replace('ASB', 1)
-        df['Logement'] = df['Logement'].replace('AAB', 2)
-        df['Logement'] = df['Logement'].replace('ML', 3)
-        df['Logement'] = df['Logement'].replace('MI', 4)
+        df['Logement'] = df['Logement'].replace('ASB', 0)
+        df['Logement'] = df['Logement'].replace('AAB', 1)
+        df['Logement'] = df['Logement'].replace('ML', 2)
+        df['Logement'] = df['Logement'].replace('MI', 3)
 
-        df['Zone'] = df['Zone'].replace('U', 1)
-        df['Zone'] = df['Zone'].replace('PU', 2)
-        df['Zone'] = df['Zone'].replace('R', 3)
+        df['Zone'] = df['Zone'].replace('U', 0)
+        df['Zone'] = df['Zone'].replace('PU', 1)
+        df['Zone'] = df['Zone'].replace('R', 2)
 
-        df = df.replace('NSP', 0)
+        df = df.replace('NSP', -1)
         df = df.astype(int)
         return df
     
-    def __inlocuieste_zero_cu_media__(self, df):
+    def __inlocuieste_minus1_cu_media__(self, df):
         for col in df.columns:
-            media_rotunjita = round(df[df[col] != 0][col].mean())
-            df[col] = df[col].replace(0, media_rotunjita)
+            media_rotunjita = round(df[df[col] != -1][col].mean())
+            df[col] = df[col].replace(-1, media_rotunjita)
         return df
     
     def __scriere_set_de_date__(self, df, fisier):
